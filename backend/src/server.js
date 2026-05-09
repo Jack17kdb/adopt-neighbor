a1477ff56dc569147cdb353f4da8c20e8aec6b14
Jack17kdb <jack@kali.Johnson.com>
2026-05-09 07:40:21 +0300

    deploy to render

--- backend/src/server.js
+++ backend/src/server.js
@@ -32,13 +32,14 @@ app.use(helmet({
     directives: {
       'default-src': ["'self'"],
       'img-src': ["'self'", 'data:', 'https://*.googlesyndication.com', 'https://*.google.com', 'https://*.doubleclick.net'],
-      'script-src': ["'self'", "'unsafe-inline'", 'https://*.googlesyndication.com', 'https://www.googletagservices.com', 'https://adservice.google.com', 'https://*.google.com', ''https://*.adtrafficquality.google', 'https://adtrafficquality.google'],
+      'script-src': ["'self'", "'unsafe-inline'", 'https://*.googlesyndication.com', 'https://www.googletagservices.com', 'https://adservice.google.com', 'https://*.google.com', 'https://*.adtrafficquality.google', 'https://adtrafficquality.google', 'https://ep2.adtrafficquality.google'],
       'frame-src': ["'self'", 'https://*.doubleclick.net', 'https://tpc.googlesyndication.com', 'https://*.google.com'],
       'connect-src': [
         "'self'",
         'https://pagead2.googlesyndication.com',
         'https://adservice.google.com',
-        'https://adtrafficquality.google'
+        'https://adtrafficquality.google',
+	'https://ep2.adtrafficquality.google',
         'https://*.adtrafficquality.google',
         'https://*.google.com'
       ],
